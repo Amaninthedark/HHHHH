8ee28351a99ac42f6d6641bf140998901589159a
Amaninthedark <741977480@qq.com>
2017-11-09 16:59:12 +0800

    11.8

---
+++ js/zongji.js
@@ -0,0 +1,179 @@
+///*******************************************************/
+//     var kfz = document.getElementById('kaifazhe');
+//
+//			kfz.addEventListener('tap', function() {
+//				//打开新窗口
+//				mui.openWindow({
+//					
+//					url: 'Release.html', //需要打开页面的url地址 
+//					id: 'Release', //需要打开页面的url页面id
+//					styles: {
+//						top: '0px', //新页面顶部位置 
+//						bottom: '0px', //新页面底部位置 
+//						//					width:newpage-width,//新页面宽度，默认为100% 
+//						//					height:newpage-height,//新页面高度，默认为100% ...... 
+//					},	
+//				});
+//			})
+//	   var kfz1= document.getElementById("kaifazhe1");
+//     kfz1.addEventListener('tap', function() {
+//				//打开新窗口
+//				mui.openWindow({
+//					
+//					url: 'Release.html', //需要打开页面的url地址 
+//					id: 'Release', //需要打开页面的url页面id
+//					styles: {
+//						top: '0px', //新页面顶部位置 
+//						bottom: '0px', //新页面底部位置 
+//						//					width:newpage-width,//新页面宽度，默认为100% 
+//						//					height:newpage-height,//新页面高度，默认为100% ...... 
+//					},	
+//				});
+//			})
+/*******************发布招聘*******************/
+          var fbzp= document.getElementById("fabuzhaopin");
+       fbzp.addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'faujiemian.html', //需要打开页面的url地址 
+					id: 'faujiemian', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+       
+       /**************************发布求职****************************/
+       var kfz2= document.getElementById("kaifazhe2");
+       kfz2.addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+       
+       
+       /**************************投稿*****************************/
+      
+       var tougao= document.getElementsByClassName("recruit-tougao");
+       tougao['0'].addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+       var tougao= document.getElementsByClassName("recruit-tougao");
+       tougao['1'].addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+       var tougao= document.getElementsByClassName("recruit-tougao");
+       tougao['2'].addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+      var tougao= document.getElementsByClassName("recruit-tougao");
+       tougao['3'].addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+       var tougao= document.getElementsByClassName("recruit-tougao");
+       tougao['4'].addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+       var tougao= document.getElementsByClassName("recruit-tougao");
+       tougao['5'].addEventListener('tap', function() {
+				//打开新窗口
+				mui.openWindow({
+					
+					url: 'application.html', //需要打开页面的url地址 
+					id: 'application', //需要打开页面的url页面id
+					styles: {
+						top: '0px', //新页面顶部位置 
+						bottom: '0px', //新页面底部位置 
+						//					width:newpage-width,//新页面宽度，默认为100% 
+						//					height:newpage-height,//新页面高度，默认为100% ...... 
+					},	
+				});
+			})
+   
+
+/*****************开发商接受信息*********************/
+function doClick(x){
+	  x.className="accept-header-left";
+	  var j;
+	 var s;
+	 var e;
+	 for(var i=1;i<=3;i++){ //这里3 需要你修改 你多少条分类 就是多少
+	   s ="nav"+i;	
+	   j=document.getElementById(s);
+	   e = document.getElementById("sub"+i);
+	   if(s != x.id){
+	   	 j.className="accept-header-Middle"
+	   	 e.style.display = "none";
+	   }else{
+			e.style.display = "block";
+	   }
+	 }
+	 }	
